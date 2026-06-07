e2ffb185ae7c3a4be7ac416938fc9bdba6693b18
AyushGarg <ayushstar76@gmail.com>
2026-06-07 20:49:04 +0530

    Created the tool

--- tools.py
+++ tools.py
@@ -1,3 +1,37 @@
-from dotenv import load_env
-load_env()
+from dotenv import load_dotenv
+load_dotenv()
 from langchain.tools import tool
+from bs4 import BeautifulSoup
+from tavily import TavilyClient
+import os
+import requests
+from rich import print
+
+tavily = TavilyClient(api_key = os.getenv("TAVILY_API_KEY"))
+
+@tool
+def web_search(query: str) -> str:
+    """Search the web for recent and reliable information on a topic. Return titles, urls and snippets."""
+    results = tavily.search(query=query, max_results=5)
+
+    out = []
+    for r in results['results']:
+        out.append(
+            f"Title: {r['title']}\n URl: {r['url']}\n Snippet: {r['content'][:300]}\n --- "
+        )
+    
+    return '\n'.join(out)
+
+@tool
+def scrape_url(url: str) -> str:
+    """Scrape and return clean text content from a given url for deep reading."""
+    try:
+        resp = requests.get(url, timeout=8, headers={'User-Agent':'Mozilla/5.0'})
+        soup = BeautifulSoup(resp.content, 'html.parser')
+        for tag in soup(["script", "style", "nav", "footer"]):
+            tag.decompose()
+        return soup.get_text(separator=" ", strip=True)[:3000]
+    except Exception as e:
+        return f"Error: {str(e)}"
+
+print(scrape_url.invoke({"url":"https://en.wikipedia.org/wiki/Middle_Eastern_crisis_(2023%E2%80%93present)"}))
